d89fb51d415c3c5ef2b2aef5e6c342fe6ccc7f1f
ivahaev <ivahaev@gmail.com>
2016-06-24 21:53:38 +0500

    filter summary beautifiing

--- src/js/components/filters/FiltersSummary.js
+++ src/js/components/filters/FiltersSummary.js
@@ -2,19 +2,19 @@
  * Created by kib357 on 06/09/15.
  */
 
-import React from 'react';
-import { displayTypes } from 'constants';
-import ValueConverter from './ValueConverter';
-import i18n from '../../stores/i18nStore';
-import filtersActions from '../../actions/filtersActuators';
-import moment from 'moment';
-import classNames from 'classnames';
+import React from "react";
+import { displayTypes } from "constants";
+import ValueConverter from "./ValueConverter";
+import i18n from "../../stores/i18nStore";
+import filtersActions from "../../actions/filtersActuators";
+import moment from "moment";
+import classNames from "classnames";
 
 class FiltersSummary extends React.Component {
     clear(e) {
         e.preventDefault();
         filtersActions.clearFilter(this.props.storeName);
-        if (typeof this.props.onClear === 'function') {
+        if (typeof this.props.onClear === "function") {
             this.props.onClear();
         }
     }
@@ -23,31 +23,47 @@ class FiltersSummary extends React.Component {
         // if (prefsStore.getUserPreference(this.props.storeName + '-show-filters')) {
         //     return null;
         // }
-        var filters = Object.keys(this.props.filters).map((filterName) => {
-            var filter = this.props.filters[filterName];
-            var desc = this.props.filtersDesc[filterName];
-            if (desc == null || filterName === '_state') {
+        let filters = Object.keys(this.props.filters).map((filterName) => {
+            let filter = this.props.filters[filterName];
+            let desc = this.props.filtersDesc[filterName];
+            if (desc == null || filterName === "_state") {
                 return null;
-            //     if (prefsStore.getUserPreference(this.props.storeName + '-show-filters')) {
-            //         return null;
-            //     }
-            //     let config = configStore.getConfig(this.props.storeName);
-            //     let label = template.render((config.states[filter] || {}).label || '', {"$i18n": i18n.getForStore(this.props.storeName)});
-            //     return (<span className="m-r-14" key={filterName}>{label}</span>);
+                //     if (prefsStore.getUserPreference(this.props.storeName + '-show-filters')) {
+                //         return null;
+                //     }
+                //     let config = configStore.getConfig(this.props.storeName);
+                //     let label = template.render((config.states[filter] || {}).label || '', {"$i18n": i18n.getForStore(this.props.storeName)});
+                //     return (<span className="m-r-14" key={filterName}>{label}</span>);
             }
+            let templateModel = { "$i18n": i18n.getForStore(this.props.storeName) };
+            let label = desc.label(templateModel);
             switch (desc.display) {
                 case displayTypes.searchBox:
-                    return (<ValueConverter key={filterName} value={filter} storeName={desc.store}/>);
+                    return (<span key={filterName}>
+                        {label}: <ValueConverter key={filterName} value={filter} storeName={desc.store}/>
+                    </span>);
                 case displayTypes.dateRange:
                     return (<span key={filterName}>
-                        {moment(filter[0]).format('L') + ' - ' + moment(filter[1]).format('L')}
+                        {label}: {moment(filter[0]).format("L") + " - " + moment(filter[1]).format("L") }
                     </span>);
                 case displayTypes.numberRange:
                     return (<span key={filterName}>
-                        {(filter[0] || '...') + ' - ' + (filter[1] || '...')}
+                        {label}: {(filter[0] || "...") + " - " + (filter[1] || "...") }
                     </span>);
+                case displayTypes.checkList: {
+                    let getValue = (val) => {
+                        for (let i in desc.options) {
+                            if (desc.options[i].value === val) {
+                                return desc.options[i].label(templateModel);
+                            }
+                        }
+                    };
+                    return (<span key={filterName}>
+                        {label}: {(filter || []).map(val => getValue(val)).join(", ")}
+                    </span>);
+                }
                 default:
-                    return (<span className="m-r-14" key={filterName}>{filter}</span>);
+                    return (<span className="m-r-14" key={filterName}>{label}: {filter}</span>);
             }
         });
         filters = filters.filter(f => f != null);
@@ -57,10 +73,10 @@ class FiltersSummary extends React.Component {
         });
         return (
             <div className={cn}>
-                <span>{i18n.get("filters.title") + ':'}</span>{filters}
+                <span>{i18n.get("filters.title") + ":"}</span>{filters}
                 { show ?
-                    <button onClick={this.clear.bind(this)}
-                            className="btn-icon first last close">
+                    <button onClick={this.clear.bind(this) }
+                        className="btn-icon first last close">
                         <i className="material-icons text md-18">close</i>
                     </button> : null }
             </div>
