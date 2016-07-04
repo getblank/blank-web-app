b7c0498ab37801bbec674f424ae70f1a819addcc
kib357 <kib357@gmail.com>
2016-07-04 16:26:55 +0500

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
         path: "./release",
         filename: "bundle.js",
-        publicPath: "/js/",
+        publicPath: "/blank/js/",
     },
     resolve: {
         root: [
