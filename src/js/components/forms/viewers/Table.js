6b0b0543a0aaa2a937bd14ac50f173c3d3b1b115
kib357 <kib357@gmail.com>
2016-07-22 10:27:34 +0500

    Table totals support

--- src/js/components/forms/viewers/Table.js
+++ src/js/components/forms/viewers/Table.js
@@ -18,7 +18,12 @@ class Table extends Component {
     }
 
     render() {
-        let header = (this.props.columns || []).map((column, index) => {
+        let totals = null, data = this.props.data, columnsDescs = (this.props.columns || []);
+        if (data.items) {
+            totals = data.totals;
+            data = data.items;
+        }
+        let header = columnsDescs.map((column, index) => {
             let className = cn({
                 "number": column.type === propertyTypes.int || column.type === propertyTypes.float,
                 "order": this.props.orderBy && this.props.orderBy.replace(/^-/, "") === column.key,
@@ -30,12 +35,17 @@ class Table extends Component {
                     onClick={column.disableOrder || this.state.loading ? null : this.orderHandler.bind(this, column.key) }>{column.label}</th>
             );
         });
-        let data = this.props.data.map((d, i) => {
+        let footer = null;
+        if (totals) {
+            footer = columnsDescs.map(c => {
+                let className = cn({ "number": [propertyTypes.int, propertyTypes.float].indexOf(c.type) >= 0 }, c.className);
+                return <td key={"td-f-" + c.key} className={className}>{totals[c.key]}</td>;
+            });
+        }
+        let rows = data.map((d, i) => {
             let columns = [];
-            for (let c of this.props.columns) {
-                let className = cn({
-                    "number": c.type === propertyTypes.int || c.type === propertyTypes.float,
-                }, c.className);
+            for (let c of columnsDescs) {
+                let className = cn({ "number": [propertyTypes.int, propertyTypes.float].indexOf(c.type) >= 0 }, c.className);
                 columns.push(<td key={"td-" + i + "-" + c.key} className={className}>{d[c.key]}</td>);
             }
             return <tr key={"tr-" + i}>{columns}</tr>;
@@ -49,8 +59,13 @@ class Table extends Component {
                             {header}
                         </tr>
                     </thead>
+                    {totals && <tfoot>
+                        <tr>
+                            {footer}
+                        </tr>
+                    </tfoot>}
                     <tbody>
-                        {data}
+                        {rows}
                     </tbody>
                 </table>
             </div>
