0be0760c7af7b99876d999d66247deb6f483e991
ivahaev <ivahaev@gmail.com>
2016-09-15 21:39:53 +0500

    fetch method for client

--- src/js/wamp/client.js
+++ src/js/wamp/client.js
@@ -3,9 +3,12 @@
  */
 
 import connectionActions from "../actions/connectionActuator.js";
+import credentialsStore from "../stores/credentialsStore.js";
 import WampClient from "wamp";
 
-var wsUrl = (process.env.WS || ((location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/")) + "wamp";
+let accessToken = credentialsStore.getApiKey();
+let suffix = accessToken ? "?access_token=" + encodeURIComponent(credentialsStore.getApiKey()) : "";
+var wsUrl = (process.env.WS || ((location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/")) + "wamp" + suffix;
 var wampClient = new WampClient(true, true);
 wampClient.onopen = function () {
     console.info("connected to " + wsUrl);
@@ -43,8 +46,24 @@ var callViaXhr = function (uri, cb) {
     xhr.send();
 };
 
+var callViaFetch = function (args, callback) {
+    let uri = `${location.protocol}//${location.host}/${args.uri}`;
+    delete args.uri;
+    args.method = args.method || "GET";
+    fetch(uri, args)
+        .then(res => {
+            if (res.status === 200 || res.status === 201) {
+                return res.json();
+            }
+            throw new Error(res.statusText);
+        })
+        .catch(callback);
+};
+
 var call = function (uri, callback) {
-    if (uri.indexOf("xhr.") === 0) {
+    if (uri.uri) {
+        callViaFetch(uri, callback);
+    } else if (uri.indexOf("xhr.") === 0) {
         callViaXhr("/" + uri.slice(4), callback);
     } else {
         wampClient.call.apply(null, arguments);
