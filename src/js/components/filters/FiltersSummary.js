43ac0cad8bb110c8e3f417682f3d5fca7530ebcc
Evgeniy Ivakha <ivahaev@gmail.com>
2017-06-09 18:33:28 +0500

    filtersSummary updated for checkbox and checkList

--- src/js/components/filters/FiltersSummary.js
+++ src/js/components/filters/FiltersSummary.js
@@ -20,22 +20,13 @@ class FiltersSummary extends React.Component {
     }
 
     render() {
-        // if (prefsStore.getUserPreference(this.props.storeName + '-show-filters')) {
-        //     return null;
-        // }
         let filters = Object.keys(this.props.filters).sort().map((filterName) => {
             let filter = this.props.filters[filterName];
             let desc = this.props.filtersDesc[filterName];
             if (desc == null || filterName === "_state") {
                 return null;
-                //     if (prefsStore.getUserPreference(this.props.storeName + '-show-filters')) {
-                //         return null;
-                //     }
-                //     let config = configStore.getConfig(this.props.storeName);
-                //     let label = template.render((config.states[filter] || {}).label || '', {"$i18n": i18n.getForStore(this.props.storeName)});
-                //     return (<span className="m-r-14" key={filterName}>{label}</span>);
             }
-            let templateModel = { "$i18n": i18n.getForStore(this.props.storeName) };
+            let templateModel = { $i18n: i18n.getForStore(this.props.storeName) };
             let label = desc.label(templateModel);
             label = (label && label !== filterName) ? (label + ":") : "";
             let getValueLabel = (val) => {
@@ -44,7 +35,7 @@ class FiltersSummary extends React.Component {
                     if (typeof desc.options[i].value === "number") {
                         matched = desc.options[i].value === val * 1;
                     } else {
-                        matched = desc.options[i].value === val * "";
+                        matched = desc.options[i].value === val + "";
                     }
                     if (matched) {
                         return desc.options[i].label(templateModel);
@@ -55,24 +46,26 @@ class FiltersSummary extends React.Component {
             switch (desc.display) {
                 case displayTypes.searchBox:
                     return (<span key={filterName}>
-                        {label} <ValueConverter key={filterName} value={filter} storeName={desc.store}/>
+                        {label} <ValueConverter key={filterName} value={filter} storeName={desc.store} />
                     </span>);
                 case displayTypes.dateRange:
                     return (<span key={filterName}>
-                        {label} {moment(filter[0]).format("L") + " - " + moment(filter[1]).format("L") }
+                        {label} {moment(filter[0]).format("L") + " - " + moment(filter[1]).format("L")}
                     </span>);
                 case displayTypes.numberRange:
                     return (<span key={filterName}>
-                        {label} {(filter[0] || "...") + " - " + (filter[1] || "...") }
+                        {label} {(filter[0] || "...") + " - " + (filter[1] || "...")}
                     </span>);
+                case displayTypes.checkbox:
+                    return <span key={filterName}>{desc.label(templateModel)}</span>;
                 case displayTypes.checkList: {
                     return (<span key={filterName}>
-                        {label} {(filter || []).map(val => getValueLabel(val)).join(", ") }
+                        {label} {(filter || []).map(val => getValueLabel(val)).join(", ")}
                     </span>);
                 }
                 case displayTypes.select:
                     return (<span key={filterName}>
-                        {label} {getValueLabel(filter) }
+                        {label} {getValueLabel(filter)}
                     </span>);
                 default:
                     return (<span className="m-r-14" key={filterName}>{label} {filter}</span>);
@@ -81,16 +74,16 @@ class FiltersSummary extends React.Component {
         filters = filters.filter(f => f != null);
         let show = filters.length > 0;
         let cn = classNames("pd-filters-summary relative no-shrink", {
-            "show": show,
+            show: show,
         });
         return (
             <div className={cn}>
                 <span>{i18n.get("filters.title") + ":"}</span>{filters}
-                { show ?
-                    <button onClick={this.clear.bind(this) }
+                {show ?
+                    <button onClick={this.clear.bind(this)}
                         className="btn-icon first last close">
                         <i className="material-icons text md-18">close</i>
-                    </button> : null }
+                    </button> : null}
             </div>
         );
     }
