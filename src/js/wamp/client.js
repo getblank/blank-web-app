6be1bba32bf69c6d1387435040283ff487382be0
Evgeniy Ivakha <ivahaev@gmail.com>
2017-07-05 15:37:34 +0500

    Revert "Using new blank-web-sdk with only cookie token provider"
    This reverts commit ce1e2e1c96264124e29f9c2f0732da8df9496ad8.

--- src/js/wamp/client.js
+++ src/js/wamp/client.js
@@ -17,37 +17,61 @@ const client = new BlankClient(uri);
 client.init()
     .then(() => {
         console.log("BlankClient initialized!");
-        return client.getTokenInfo()
-            .then(tokenInfo => {
-                if (tokenInfo) {
-                    credentialsActions.setUser({ _id: tokenInfo._id });
+        let tokenInfo = client.getTokenInfo();
+        if (tokenInfo) {
+            credentialsActions.setUser({ _id: tokenInfo.userId });
+        }
+        client.on("change", (state, prevState) => {
+            switch (state) {
+                case "wsConnecting": {
+                    connectionActions.disconnected();
+                    const info = client.getTokenInfo();
+                    credentialsActions.updateUserData({ _id: info.userId });
+                    break;
                 }
-
-                client.on("change", (state, prevState) => {
-                    switch (state) {
-                        case "wsConnecting": {
-                            connectionActions.disconnected();
-                            client.getTokenInfo()
-                                .then(info => {
-                                    credentialsActions.updateUserData({ _id: info.userId });
-                                });
-                            break;
-                        }
-                        case "wsConnected":
-                            connectionActions.connected();
-                            break;
-                        case "unauthorized":
-                            if (state !== prevState) {
-                                connectionActions.disconnected();
-                                credentialsActions.clearUserData();
-                            }
-                            break;
-                        default:
-                            connectionActions.disconnected();
-                            break;
+                case "wsConnected":
+                    connectionActions.connected();
+                    break;
+                case "unauthorized":
+                    if (state !== prevState) {
+                        connectionActions.disconnected();
+                        credentialsActions.clearUserData();
                     }
-                });
-            });
+                    break;
+                default:
+                    connectionActions.disconnected();
+                    break;
+            }
+        });
     });
+
+// window.addEventListener("beforeunload", function () {
+//     wampClient.close(null, false);
+// });
+
+// var callViaFetch = function (args, callback) {
+//     let uri = `${location.protocol}//${location.host + location.pathname + args.uri}`;
+//     delete args.uri;
+//     args.method = args.method || "GET";
+//     fetch(uri, args)
+//         .then(res => {
+//             if (res.status === 200 || res.status === 201) {
+//                 return res.json();
+//             }
+//             throw new Error(res.statusText);
+//         })
+//         .then(res => { callback(null, res) })
+//         .catch(callback);
+// };
+
+// var call = function (uri) {
+//     let data = Array.prototype.slice.call(arguments, 1);
+//     let callback = typeof data[data.length - 1] === "function" ? data.pop() : () => { };
+//     if (uri.uri) {
+//         callViaFetch(uri, callback);
+//     } else {
+//         client.call.apply(wampClient, arguments);
+//     }
+// };
 
 export default client;
