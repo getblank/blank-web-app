469fba050fb89e2a8d5d126d9ec02d748be3f04a
Evgeniy Ivakha <ivahaev@gmail.com>
2017-10-09 13:33:09 +0500

    styling

--- src/js/components/auth/Locales.js
+++ src/js/components/auth/Locales.js
@@ -9,7 +9,7 @@ import configActions from "../../actions/configActuators.js";
 import credentialsActions from "../../actions/credentialsActuators.js";
 import { storeEvents, lsKeys } from "constants";
 
-var converter = new Map([["ru", "Русский"], ["en", "English"]]);
+const converter = new Map([["ru", "Русский"], ["en", "English"]]);
 
 class Locales extends React.Component {
     constructor(props) {
@@ -19,18 +19,20 @@ class Locales extends React.Component {
     }
 
     getStateFromStores() {
-        var res = {"locales": [], "current": ""};
-        var user = credentialsStore.getUser();
+        const res = { locales: [], current: "" };
+        const user = credentialsStore.getUser();
         res.current = user != null ? user.lang : localStorage.getItem(lsKeys.locale);
         res.locales = configStore.getLocales();
         if (!res.current) {
             res.current = configStore.getDefaultLocale();
         }
+
         return res;
     }
 
     setLocale(locale) {
-        var user = credentialsStore.getUser();
+        const user = credentialsStore.getUser();
+        console.info("LOCALE", locale, user);
         if (user == null) {
             configActions.setLocale(locale);
         } else {
@@ -43,14 +45,14 @@ class Locales extends React.Component {
     }
 
     render() {
-        var items = null;
+        let items = null;
         if (this.state.locales.length > 1) {
             items = this.state.locales.map((item, i) => (
                 <button type="button"
-                        key={item}
-                        className={"btn-flat" + (i === 0 ? " first": "") + (i === this.state.locales.length - 1 ? " last": "")}
-                        disabled={item === this.state.current}
-                        onClick={this.setLocale.bind(this, item)}>
+                    key={item}
+                    className={"btn-flat" + (i === 0 ? " first" : "") + (i === this.state.locales.length - 1 ? " last" : "")}
+                    disabled={item === this.state.current}
+                    onClick={this.setLocale.bind(this, item)}>
                     {this.convert(item)}
                 </button>
             ));
