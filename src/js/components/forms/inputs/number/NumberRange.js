ed6a14548e6c22ab763b0129e5d69afdb61796cf
Evgeniy Ivakha <ivahaev@gmail.com>
2018-03-26 18:05:30 +0500

    NumberRange now is self controlled

--- src/js/components/forms/inputs/number/NumberRange.js
+++ src/js/components/forms/inputs/number/NumberRange.js
@@ -2,12 +2,39 @@
  * Created by kib357 on 08/02/16.
  */
 
-import React from 'react';
+import React from "react";
+
+const rgx = /[^0-9.]/g;
 
 class NumberRange extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        if (Array.isArray(props.value)) {
+            const [from, to] = props.value;
+            this.state = { from, to };
+            return;
+        }
+
+        this.state = { from: "", to: "" };
+    }
+
+    updateValues(props) {
+        if (Array.isArray(props.value)) {
+            const [from, to] = props.value;
+            this.setState({ from, to });
+
+            return;
+        }
+
+        this.setState({ from: "", to: "" });
+    }
+
+    componentWillReceiveProps(props) {
+        if (JSON.stringify(props.value) !== JSON.stringify(this.props.value)) {
+            this.updateValues(props);
+        }
+
+        return false;
     }
 
     isValidValue(v) {
@@ -15,42 +42,40 @@ class NumberRange extends React.Component {
     }
 
     handleChange(e) {
-        let from = this.refs.from.value.replace(',', '.'),
-            to = this.refs.to.value.replace(',', '.');
+        const from = this.refs.from.value.replace(",", ".").replace(rgx, "");
+        const to = this.refs.to.value.replace(",", ".").replace(rgx, "");
         if (!this.isValidValue(from) || !this.isValidValue(to)) {
             return;
         }
+
         if (from.length === 0 && to.length === 0) {
-            console.log("nullllll");
             this.props.onChange(null);
             return;
         }
+
         this.props.onChange([from, to]);
     }
 
     render() {
-        let from, to;
-        if (Array.isArray(this.props.value)) {
-            from = this.props.value[0];
-            to = this.props.value[1];
-        }
+        const { from, to } = this.state;
+
         return (
             <div className="flex align-center">
                 <input type="text"
-                       ref="from"
-                       value={from}
-                       onChange={this.handleChange.bind(this)}
-                       className="form-control"
-                       disabled={this.props.disabled}
-                       style={{"textAlign": "right", "overflow": "hidden"} /*overflow:hidden fixes FF bug*/}/>
+                    ref="from"
+                    value={from}
+                    onChange={this.handleChange.bind(this)}
+                    className="form-control"
+                    disabled={this.props.disabled}
+                    style={{ textAlign: "right", overflow: "hidden" } /*overflow:hidden fixes FF bug*/} />
                 <span>&nbsp;&mdash;&nbsp;</span>
                 <input type="text"
-                       ref="to"
-                       value={to}
-                       onChange={this.handleChange.bind(this)}
-                       className="form-control"
-                       disabled={this.props.disabled}
-                       style={{"overflow": "hidden"}}/>
+                    ref="to"
+                    value={to}
+                    onChange={this.handleChange.bind(this)}
+                    className="form-control"
+                    disabled={this.props.disabled}
+                    style={{ overflow: "hidden" }} />
             </div>
         );
     }
