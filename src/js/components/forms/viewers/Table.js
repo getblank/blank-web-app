e128d2b520e54ab5298fbeca47a790311a255bf8
Evgeniy Ivakha <ivahaev@gmail.com>
2021-03-19 00:02:04 +0500

    DataTable improvements

--- src/js/components/forms/viewers/Table.js
+++ src/js/components/forms/viewers/Table.js
@@ -1,6 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import cn from "classnames";
-import {propertyTypes} from "constants";
+import config from "../../../stores/configStore";
+import historyActions from "../../../actions/historyActuators";
+import { propertyTypes } from "constants";
 
 class Table extends Component {
     constructor(props) {
@@ -12,61 +14,103 @@ class Table extends Component {
 
     orderHandler(column) {
         if (typeof this.props.onOrder === "function") {
-            var desc = this.props.orderBy === column;
+            const desc = this.props.orderBy === column;
             this.props.onOrder((desc ? "-" : "") + column);
         }
     }
 
+    linkClickHandler(storeName, itemId) {
+        return (e) => {
+            e.preventDefault();
+            historyActions.goToStoreItem(storeName, itemId);
+        };
+    }
+
     render() {
-        let totals = null, data = this.props.data, columnsDescs = (this.props.columns || []);
+        let totals = null;
+        let { data } = this.props;
+        const columnsDescs = this.props.columns ?? [];
         if (data.items) {
             totals = data.totals;
             data = data.items;
         }
-        let header = columnsDescs.map((column, index) => {
-            let className = cn({
-                "number": column.type === propertyTypes.int || column.type === propertyTypes.float,
-                "order": this.props.orderBy && this.props.orderBy.replace(/^-/, "") === column.key,
-                "desc": this.props.orderBy && this.props.orderBy === "-" + column.key,
-                "sortable": !column.disableOrder,
-            }, column.className);
+        const header = columnsDescs.map((column, index) => {
+            const className = cn(
+                {
+                    number: column.type === propertyTypes.int || column.type === propertyTypes.float,
+                    order: this.props.orderBy && this.props.orderBy.replace(/^-/, "") === column.key,
+                    desc: this.props.orderBy && this.props.orderBy === "-" + column.key,
+                    sortable: !column.disableOrder,
+                },
+                column.className,
+            );
             return (
-                <th className={className} key={column.key + "-" + index}
-                    onClick={column.disableOrder || this.state.loading ? null : this.orderHandler.bind(this, column.key) }>{column.label}</th>
+                <th
+                    className={className}
+                    key={column.key + "-" + index}
+                    onClick={
+                        column.disableOrder || this.state.loading ? null : this.orderHandler.bind(this, column.key)
+                    }
+                >
+                    {column.label}
+                </th>
             );
         });
         let footer = null;
         if (totals) {
-            footer = columnsDescs.map(c => {
-                let className = cn({ "number": [propertyTypes.int, propertyTypes.float].indexOf(c.type) >= 0 }, c.className);
-                return <td key={"td-f-" + c.key} className={className}>{totals[c.key]}</td>;
+            footer = columnsDescs.map((c) => {
+                const className = cn(
+                    { number: [propertyTypes.int, propertyTypes.float].indexOf(c.type) >= 0 },
+                    c.className,
+                );
+                return (
+                    <td key={"td-f-" + c.key} className={className}>
+                        {totals[c.key]}
+                    </td>
+                );
             });
         }
-        let rows = data.map((d, i) => {
-            let columns = [];
-            for (let c of columnsDescs) {
-                let className = cn({ "number": [propertyTypes.int, propertyTypes.float].indexOf(c.type) >= 0 }, c.className);
-                columns.push(<td key={"td-" + i + "-" + c.key} className={className}>{d[c.key]}</td>);
+        const rows = data.map((item, i) => {
+            const columns = [];
+            for (const column of columnsDescs) {
+                const className = cn(
+                    { number: [propertyTypes.int, propertyTypes.float].indexOf(column.type) >= 0 },
+                    column.className,
+                );
+                let text = item[column.key];
+                if (column.tableLink) {
+                    const storeName = column.store ? column.store : this.props.storeName;
+                    const itemId = column.foreignKey ? item[column.foreignKey] : item._id;
+                    const to = config.findRoute(storeName) + "/" + itemId;
+                    text = (
+                        <a href={to} onClick={this.linkClickHandler(storeName, itemId)}>
+                            {text}
+                        </a>
+                    );
+                }
+
+                columns.push(
+                    <td key={"td-" + i + "-" + column.key} className={className}>
+                        {text}
+                    </td>,
+                );
             }
             return <tr key={"tr-" + i}>{columns}</tr>;
         });
-        let className = cn("pd-data-table", this.props.className);
+
+        const className = cn("pd-data-table", this.props.className);
         return (
-            <div style={{ "overflowX": "auto" }}>
+            <div style={{ overflowX: "auto" }}>
                 <table className={className}>
                     <thead>
-                        <tr>
-                            {header}
-                        </tr>
+                        <tr>{header}</tr>
                     </thead>
-                    {totals && <tfoot>
-                        <tr>
-                            {footer}
-                        </tr>
-                    </tfoot>}
-                    <tbody>
-                        {rows}
-                    </tbody>
+                    {totals && (
+                        <tfoot>
+                            <tr>{footer}</tr>
+                        </tfoot>
+                    )}
+                    <tbody>{rows}</tbody>
                 </table>
             </div>
         );
